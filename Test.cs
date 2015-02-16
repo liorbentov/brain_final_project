12521e66ee0f80e3638449b1f3455fd8507e9478
LiorBentov <liorbentov@gmail.com>
2015-02-16 18:49:56 +0200

    add excel

--- Test.cs
+++ Test.cs
@@ -7,14 +7,18 @@ using System.Threading.Tasks;
 
 namespace FinalProject
 {
+    /// <summary>
+    /// This class represents the Test
+    /// </summary>
     class Test
     {
+        private const string CSV_SEPEARATOR = ";";
+
         private string m_strUserID;
         private double m_dScore;
         private double m_dTime;
         private DateTime m_dtDate;
         private QuestionScore[] m_questions = new QuestionScore[7];
-        private Form1 questionsForm;
 
 
         public string UserID { get { return this.m_strUserID; } set { this.m_strUserID = value; } }
@@ -22,20 +26,48 @@ namespace FinalProject
         public double Time { get { return this.m_dTime; } set { this.m_dTime = value; } }
         public DateTime Date { get { return this.m_dtDate; } set { this.m_dtDate = value; } }
 
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public Test()
         {
             this.Date = DateTime.Now;
-            questionsForm = new Form1();
         }
 
+        /// <summary>
+        /// Start the test (Questionaire)
+        /// </summary>
         public void startTest()
         {
-            this.questionsForm.Show();
         }
 
+        /// <summary>
+        /// Save the current question score
+        /// </summary>
+        /// <param name="index">Question Index (1-7)</param>
+        /// <param name="score">Question Score</param>
         public void saveScore(int index, QuestionScore score)
         {
-            this.m_questions[index] = score;
+            this.m_questions[index - 1] = score;
+        }
+
+        /// <summary>
+        /// Save the test results to file
+        /// </summary>
+        private void saveTestToFile()
+        {
+            // Set the data to one line
+            string strData = Date.ToShortDateString() +
+                CSV_SEPEARATOR + Date.ToShortTimeString() +
+                CSV_SEPEARATOR + UserID +
+                CSV_SEPEARATOR;
+            
+
+            // Open file of tests
+
+            // Write the line in the file
+
+            // Close the file
         }
     }
 
--- TestStart.Designer.cs
+++ TestStart.Designer.cs
@@ -32,6 +32,14 @@
             this.btnStartTest = new System.Windows.Forms.Button();
             this.txtUserID = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label6
@@ -39,7 +47,7 @@
             this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.label6.Location = new System.Drawing.Point(96, 9);
+            this.label6.Location = new System.Drawing.Point(49, 15);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(102, 19);
             this.label6.TabIndex = 12;
@@ -50,7 +58,7 @@
             // 
             this.btnStartTest.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.btnStartTest.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.btnStartTest.Location = new System.Drawing.Point(109, 119);
+            this.btnStartTest.Location = new System.Drawing.Point(62, 125);
             this.btnStartTest.Name = "btnStartTest";
             this.btnStartTest.Size = new System.Drawing.Size(75, 23);
             this.btnStartTest.TabIndex = 13;
@@ -61,7 +69,7 @@
             // txtUserID
             // 
             this.txtUserID.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.txtUserID.Location = new System.Drawing.Point(98, 94);
+            this.txtUserID.Location = new System.Drawing.Point(51, 100);
             this.txtUserID.Name = "txtUserID";
             this.txtUserID.Size = new System.Drawing.Size(100, 21);
             this.txtUserID.TabIndex = 14;
@@ -70,29 +78,95 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
-            this.label1.Location = new System.Drawing.Point(116, 75);
+            this.label1.Location = new System.Drawing.Point(69, 81);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(55, 13);
             this.label1.TabIndex = 15;
             this.label1.Text = "הכנס ת.ז";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnStartTest);
+            this.panel1.Controls.Add(this.txtUserID);
+            this.panel1.Location = new System.Drawing.Point(43, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 169);
+            this.panel1.TabIndex = 16;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(43, 187);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 162);
+            this.panel2.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(69, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 19);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "תוצאות";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label3.Location = new System.Drawing.Point(115, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "ציון";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.button1.Location = new System.Drawing.Point(45, 110);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 23);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "התחל מבחן חדש";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label4.Location = new System.Drawing.Point(116, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(23, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "זמן";
             // 
             // TestStart
             // 
             this.AcceptButton = this.btnStartTest;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtUserID);
-            this.Controls.Add(this.btnStartTest);
-            this.Controls.Add(this.label6);
+            this.ClientSize = new System.Drawing.Size(284, 361);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "TestStart";
             this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.RightToLeftLayout = true;
             this.Text = "MMSE";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -102,5 +176,11 @@
         private System.Windows.Forms.Button btnStartTest;
         private System.Windows.Forms.TextBox txtUserID;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
     }
 }

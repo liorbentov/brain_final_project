ff42190d1eefa9c1cdbf5b1bfd7d0d812b0bda3c
yotamgever <yotam.rt@gmail.com>
2015-03-08 19:40:19 +0200

    bbb

---
+++ TestStartFinish.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject
+{
+    public partial class TestStartFinish : Form
+    {
+        private Test tNew;
+        public TestStartFinish()
+        {
+            InitializeComponent();
+            this.txtUserID.Select();
+        }
+
+        private void btnStartTest_Click(object sender, EventArgs e)
+        {
+            if (this.txtUserID.Text == string.Empty)
+            {
+                MessageBox.Show("יש להזין תעודת זהות על מנת להתחיל את המבחן");
+            }
+            else 
+            {
+                // Create new Test
+                tNew = new Test(DateTime.Now, this.txtUserID.Text, 0, 0);
+
+                // Display questions
+                Questionaire questionaire = new Questionaire(tNew);
+                questionaire.FormClosed += questionaire_FormClosed;
+                questionaire.Show();
+
+                this.Hide();
+            }
+        }
+
+        void questionaire_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (!tNew.isOver())
+            {
+                this.Close();
+            }
+            else
+            {
+                this.Show();
+                pNewTest.Visible = false;
+                pResults.Visible = true;
+
+                // Calculate this test's results
+                tNew.endTest();
+                this.lblScore.Text = tNew.Score.ToString();
+                this.lblTime.Text = tNew.Time.ToString();
+
+                // Get user's previous tests
+                tNew.User.getUserTests();
+
+                // Check if there are previous test
+                if (tNew.User.PreviousTest.Count == 0)
+                {
+                    this.pTestResults.Location = new Point(54, 40);
+                    this.pAvg.Visible = false;
+                }
+                else
+                {
+                    this.pTestResults.Location = new Point(101, 49);
+                    this.pAvg.Visible = true;
+
+                    // Set the avg and the diff
+                    this.lblScoreAvg.Text = setStringForResult(tNew.User.ScoreAvearage.ToString());
+                    this.lblTimeAvg.Text = setStringForResult(tNew.User.TimeAvearage.ToString());
+                    this.lblScoreDiff.Text = setStringForResult((tNew.Score - tNew.User.ScoreAvearage).ToString());
+                    this.lblTimeDiff.Text = setStringForResult((tNew.Time - tNew.User.TimeAvearage).ToString());
+
+                    // Set diff color
+                    if ((tNew.Score - tNew.User.ScoreAvearage) < 0)
+                    {
+                        this.lblScoreDiff.ForeColor = Color.Red;
+                    }
+                    else
+                    {
+                        if (tNew.Score == tNew.User.ScoreAvearage)
+                        {
+                            this.lblScoreDiff.ForeColor = Color.Blue;
+                        }
+                        else
+                        {
+                            this.lblScoreDiff.ForeColor = Color.Green;
+                        }
+                    }
+
+                    if ((tNew.Time - tNew.User.TimeAvearage) > 0)
+                    {
+                        this.lblTimeDiff.ForeColor = Color.Red;
+                    }
+                    else
+                    {
+                        if (tNew.Time == tNew.User.TimeAvearage)
+                        {
+                            this.lblTimeDiff.ForeColor = Color.Blue;
+                        }
+                        else
+                        {
+                            this.lblTimeDiff.ForeColor = Color.Green;
+                        }
+                    }
+                }
+
+                // Save results
+                tNew.saveTestToFile();
+            }
+        }
+
+        private void btnHistory_Click(object sender, EventArgs e)
+        {
+            tNew.User.getUserTests();
+            new UserTestLog(tNew.User.PreviousTest).Show();
+            new UserLogChart(tNew.User.PreviousTest).Show();
+        }
+
+        private void btnNewTest_Click(object sender, EventArgs e)
+        {
+            btnStartTest_Click(sender, e);
+        }
+
+        private string setStringForResult(string strToShow)
+        {
+            if (strToShow.Length > 6)
+            {
+                return (strToShow.Remove(6));
+            }
+                
+            return (strToShow);
+        }
+    }
+}
---
+++ User.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    public class User
+    {
+        enum FileFields
+        {
+            TestDate,
+            TestTime,
+            UserID,
+            Score,
+            Time
+        };
+
+        private string m_strID;
+        private List<Test> m_lPreviousTest;
+        private double m_dScoreAvearage;
+        private double m_dTimeAvearage;
+
+        public string ID { get { return this.m_strID; } }
+        public List<Test> PreviousTest { get { return m_lPreviousTest; } set { m_lPreviousTest = value; } }
+        public double ScoreAvearage { get { return m_dScoreAvearage; } set { m_dScoreAvearage = value; } }
+        public double TimeAvearage { get { return m_dTimeAvearage; } set { m_dTimeAvearage = value; } }
+
+        public User(string ID)
+        {
+            this.m_strID = ID;
+            this.ScoreAvearage = 0;
+            this.TimeAvearage = 0;
+            this.PreviousTest = new List<Test>();
+        }
+
+        public void getUserTests()
+        {
+            string currLine;
+            int nTestCounter = 0;
+            double dScores = 0;
+            double dTime = 0;
+
+            // Clear list
+            this.PreviousTest.Clear();
+
+            // Open file
+            FileStream fsTestResults = new FileStream(Test.RESULTS_FILE_NAME, FileMode.OpenOrCreate, FileAccess.Read);
+            StreamReader srReader = new StreamReader(fsTestResults);
+
+            // Read the first line because it is the header
+            if (!srReader.EndOfStream)
+            {
+                srReader.ReadLine();
+            }
+
+            // Read the data and differs the data of the current user
+            while (!srReader.EndOfStream)
+            {
+                // Read the next line
+                currLine = srReader.ReadLine();
+
+                // Check if the row belongs to the current user
+                string[] data = currLine.Split(Test.CSV_SEPARATOR);
+
+                // If so, insert the data to arrays
+                if (data[(int)FileFields.UserID].Equals(this.m_strID))
+                {
+                    Test tCurr = new Test();
+                    tCurr.Date = convertStringsToDate(data[(int)FileFields.TestDate], data[(int)FileFields.TestTime]);
+                    tCurr.UserID = data[(int)FileFields.UserID];
+                    tCurr.Score = double.Parse(data[(int)FileFields.Score]);
+                    tCurr.Time = double.Parse(data[(int)FileFields.Time]);
+
+                    // Insert the test to the list
+                    m_lPreviousTest.Add(tCurr);
+
+                    // Add the score and the time for the average calculation
+                    dScores += double.Parse(data[(int)FileFields.Score]);
+                    dTime += double.Parse(data[(int)FileFields.Time]);
+
+                    nTestCounter++;
+                }
+            }
+
+            // Close file
+            srReader.Close();
+            fsTestResults.Close();
+
+            // Calc Average
+            m_dScoreAvearage = dScores / nTestCounter;
+            m_dTimeAvearage = dTime / nTestCounter;
+        }
+
+        private DateTime convertStringsToDate(string strDate, string strTime)
+        {
+            return (new DateTime(Int32.Parse(strDate.Split('/')[2]),
+                Int32.Parse(strDate.Split('/')[1]),
+                Int32.Parse(strDate.Split('/')[0]),
+                Int32.Parse(strTime.Split(':')[0]),
+                Int32.Parse(strTime.Split(':')[1]), 0));
+        }
+    }
+}
